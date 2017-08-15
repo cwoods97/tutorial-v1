fc3cbb82989855fc47e07ea8239b0f1a29dbe0e1
Clara Woods <cmwoods@google.com>
2017-08-15 10:50:41 -0700

    deleted contributor stuff

--- tutorial/main.py
+++ tutorial/main.py
@@ -32,14 +32,7 @@ class Notification(ndb.Model):
 class ThumbnailReference(ndb.Model):
   thumbnail_name = ndb.StringProperty()
   thumbnail_key = ndb.StringProperty()
-  poster_email_address = ndb.StringProperty()
   date = ndb.DateTimeProperty(auto_now_add=True)
-
-# A contributor has an email address and
-# a profile picture, which is a thumbnail reference.
-class Contributor(ndb.Model):
-  email = ndb.StringProperty()
-  profile_pic = ndb.StructuredProperty(ThumbnailReference)
 
 # Home/news feed page (notification listing).
 class MainHandler(webapp2.RequestHandler):
@@ -68,18 +61,6 @@ class PhotosHandler(webapp2.RequestHandler):
     # Write to appropriate html file
     self.response.write(template.render(template_values))
 
-# Contributors page (contributor emails).
-# LATER: also display contributor profile pics. Get
-# thumbnail reference from given contributor and use it
-# to create thumbnail array, then format in HTML file similarly
-# to in photos.html
-class ContributorsHandler(webapp2.RequestHandler):
-  def get(self):
-    contributors = Contributor.query().fetch()
-    template_values = {'contributors':contributors}
-    template = jinja_environment.get_template("contributors.html")
-    self.response.write(template.render(template_values))
-
 # For receiving Cloud Pub/Sub push messages.
 class ReceiveMessage(webapp2.RequestHandler):
   def post(self):
@@ -94,40 +75,11 @@ class ReceiveMessage(webapp2.RequestHandler):
     generation_number = str(attributes.get('objectGeneration'))
     overwrote_generation = attributes.get('overwroteGeneration')
     overwritten_by_generation = attributes.get('overwrittenByGeneration')
-    email = attributes.get('requesterEmailAddress')
-
-    # Add known contributors to datastore if not already added.
-    # Contributors are those who have performed actions on the album,
-    # not necessarily just those who have uploaded photos.
-    if email is None:
-      email = 'Unknown'
-    else:
-      # Only add contributor if not already in datastore.
-      this_contributor = Contributor.query(email=email).fetch()
-      if len(this_contributor.keys()) == 0:
-        # Specify some default photo to create contributor with here
-        new_contributor = Contributor(email=email)
-        new_contributor.put()
 
     index = photo_name.index(".jpg")
     thumbnail_key = photo_name[:index] + generation_number + photo_name[index:]
 
-    # Check to make sure user is not attempting invalid profile picture upload.
-    # If so, send specialized notification and do not perform any
-    # action with thumbnails or photos.
-    if photo_name.startswith('profile-'):
-        new_notification = create_notification(photo_name, email, invalid_profile=True)
-        new_notification.put()
-        return
-
-    # Check if user is attempting to upload profile picture for themself.
-    expected_profile_pic = 'profile-' + email + '.jpg'
-    profile = False
-    if photo_name == expected_profile_pic:
-      is_profile = True # Might be repetitive with following line
-      new_notification = create_notification(photo_name, event_type, email, generation_number, overwrote_generation=overwrote_generation, overwritten_by_generation=overwritten_by_generation, profile=True)
-    else:
-      new_notification = create_notification(photo_name, event_type, email, generation_number, overwrote_generation=overwrote_generation, overwritten_by_generation=overwritten_by_generation)
+    new_notification = create_notification(photo_name, event_type, generation_number, overwrote_generation=overwrote_generation, overwritten_by_generation=overwritten_by_generation)
     notifications = Notification.query().fetch()
     for notification in notifications:
       if new_notification == notification:
@@ -141,48 +93,34 @@ class ReceiveMessage(webapp2.RequestHandler):
     if event_type == 'OBJECT_FINALIZE':
       thumbnail = create_thumbnail(self, photo_name)
       store_thumbnail_in_gcs(self, thumbnail_key, thumbnail) # store under name thumbnail_key. Not implemented
-      thumbnail_reference = ThumbnailReference(thumbnail_name=photo_name, thumbnail_key=thumbnail_key, poster_email_address=email)
+      thumbnail_reference = ThumbnailReference(thumbnail_name=photo_name, thumbnail_key=thumbnail_key)
       thumbnail_reference.put()
-      if profile:
-        # Update contributor info
-        contributor = Contributor.query(email=email)
-        contributor.profile_pic = thumbnail_reference
-        contributor.put()
 
     # If delete/archive message: delete thumbnail from gcs bucket and delete
     # thumbnail reference from datastore.
-      filename = '/' + THUMBNAIL_BUCKET + '/' + thumbnail_key
     elif event_type == 'OBJECT_DELETE' or event_type == 'OBJECT_ARCHIVE':
       delete_thumbnail(thumbnail_key)
     # No action performed if event_type is OBJECT_UPDATE
 
 # Create notification
-def create_notification(photo_name, event_type, requester_email_address, generation, overwrote_generation=None, overwritten_by_generation=None, profile=False, invalid_profile=False):
-  if invalid_profile:
-    message = 'Invalid profile picture update attempted: ' + photo_name + '.'
-  elif profile:
-    if event_type == 'OBJECT_FINALIZE':
-      message = requester_email_address + ' uploaded a new profile picture.'
-    elif event_type == 'OBJECT_ARCHIVE' or event_type == 'OBJECT_DELETE':
-      message = requester_email_address + ' has removed their old profile picture.'
-  else:
-    if event_type == 'OBJECT_FINALIZE':
-      if overwrote_generation is not None:
-        message = photo_name + ' was uploaded by ' + requester_email_address + ' and overwrote an older version of itself.'
-      else:
-        message = requester_email_address + ' uploaded ' + photo_name + '.'
-    elif event_type == 'OBJECT_ARCHIVE':
-      if overwritten_by_generation is not None:
-        message = photo_name + ' was overwritten by a newer version, uploaded by ' + requester_email_address + '.'
-      else:
-        message = photo_name + ' was archived by ' + requester_email_address + '.'
-    elif event_type == 'OBJECT_DELETE':
-      if overwritten_by_generation is not None:
-        message = photo_name + ' was overwritten by a newer version, uploaded by ' + requester_email_address + '.'
-      else:
-        message = photo_name + ' was deleted by ' + requester_email_address + '.'
+def create_notification(photo_name, event_type, generation, overwrote_generation=None, overwritten_by_generation=None):
+  if event_type == 'OBJECT_FINALIZE':
+    if overwrote_generation is not None:
+      message = photo_name + ' was uploaded and overwrote an older version of itself.'
     else:
-      message = 'The metadata of ' + photo_name + ' was updated by ' + requester_email_address + '.'
+      message = photo_name + ' was uploaded.'
+  elif event_type == 'OBJECT_ARCHIVE':
+    if overwritten_by_generation is not None:
+      message = photo_name + ' was overwritten by a newer version.'
+    else:
+      message = photo_name + ' was archived.'
+  elif event_type == 'OBJECT_DELETE':
+    if overwritten_by_generation is not None:
+      message = photo_name + ' was overwritten by a newer version.'
+    else:
+      message = photo_name + ' was deleted.'
+  else:
+    message = 'The metadata of ' + photo_name + ' was updated.'
 
   return Notification(message=message, generation=generation)
 
@@ -219,6 +157,5 @@ def delete_thumbnail(thumbnail_key):
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/photos', PhotosHandler),
-    ('/contributors', ContributorsHandler),
     ('/_ah/push-handlers/receive_message', ReceiveMessage)
 ], debug=True)
